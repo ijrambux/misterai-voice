6fa683a20d91455b2874d736a15576b616864a19
Mouse <ijrambux@gmail.com>
2025-11-19 22:40:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,44 @@
-import express from "express";
-import http from "http";
-import { Server } from "socket.io";
-
+const express = require("express");
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+    maxHttpBufferSize: 1e8
+});
 
 app.use(express.static("public"));
 
 let users = [];
 
 io.on("connection", socket => {
-    
-    socket.on("join", user => {
-        socket.userName = user;
-        users.push(user);
 
-        io.emit("systemMessage", `🔵 ${user} انضم إلى الدردشة`);
-        io.emit("updateUsers", users);
+    socket.on("join", username => {
+        socket.username = username;
+        users.push(username);
+        io.emit("users", users);
     });
 
-    socket.on("sendMessage", data => {
-        io.emit("receiveMessage", data);
+    socket.on("chatMessage", msg => {
+        io.emit("chatMessage", { user: socket.username, msg });
     });
 
-    socket.on("voiceData", data => {
-        socket.broadcast.emit("voiceData", data);
+    socket.on("voice", blob => {
+        io.emit("voice", blob);
+    });
+
+    socket.on("speaking", user => {
+        io.emit("speaking", user);
+    });
+
+    socket.on("stopSpeaking", user => {
+        io.emit("stopSpeaking", user);
     });
 
     socket.on("disconnect", () => {
-        if (socket.userName) {
-            users = users.filter(u => u !== socket.userName);
-            io.emit("systemMessage", `🔴 ${socket.userName} غادر الدردشة`);
-            io.emit("updateUsers", users);
-        }
+        users = users.filter(u => u !== socket.username);
+        io.emit("users", users);
     });
+
 });
 
-server.listen(3000, () => {
-    console.log("🚀 Server running on port 3000");
-});
+http.listen(3000, () => console.log("🔥 Server running on :3000"));
+
